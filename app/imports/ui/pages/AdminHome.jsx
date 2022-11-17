8d6fb9fc5258aa76ec92a2ec378e9876eb983245
ChronoTrigger64 <sydneey@hawaii.edu>
2022-11-17 10:14:17 -1000

    committed a literal comment

--- app/imports/ui/pages/AdminHome.jsx
+++ app/imports/ui/pages/AdminHome.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-
+/* admin home page should have a received reports list for complaints. */
 const AdminHome = () => (
   <Container>
     <h1>Admin Home</h1>
