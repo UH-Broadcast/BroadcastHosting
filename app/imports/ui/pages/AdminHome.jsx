1b1538e3c6223bef65403b715c2891042401b5fb
ChronoTrigger64 <sydneey@hawaii.edu>
2022-11-17 18:29:24 -1000

    added columns for name of person and their complaint or suggestion to admins

--- app/imports/ui/pages/AdminHome.jsx
+++ app/imports/ui/pages/AdminHome.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Table } from 'react-bootstrap';
 /* admin home page should have a received reports list for complaints. */
 const AdminHome = () => (
-  <Container>
+  <Container id="adminhome" fluid className="py-3 align-middle text-center">
     <h1>Admin Home</h1>
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Comment/Suggestion/Complaint</th>
+          <th>Condition</th>
+        </tr>
+      </thead>
+      <tbody />
+    </Table>
   </Container>
 );
 
